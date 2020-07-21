975dfc5f9ad15cda194fe0706c25bc7863430c4b
Mario Costa <mario@watermelon.co>
2020-07-21 11:06:54 +0200

    [EXERCISE] 30 days - day 0

---
+++ 30_days_code/day1_data_types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+my_int = int(input())
+my_double = float(input())
+my_string = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + my_int)
+
+# Print the sum of the double variables on a new line.
+print(d + my_double)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(f"{s}{my_string}")
