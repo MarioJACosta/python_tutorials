00e403e87459f297ce50f9179a9929c4718f4e76
Mario Costa <mario@watermelon.co>
2020-08-29 15:22:23 +0200

    [TASK] Day 2

---
+++ hackerrank/10_days_statistic/day2_basic_probability.py
@@ -0,0 +1,16 @@
+# Question 1
+# Find the probability of getting an odd number when rolling a 6-sided fair die.
+
+def question_one():
+    total_outcomes = {1, 2, 3, 4, 5, 6}
+    favorable_outcomes = {1, 3, 5}
+
+    result = len(favorable_outcomes) / len(total_outcomes)
+
+    print(result)
+
+# Question 2
+# Find the probability of getting 1 head and 1 tail when 2 fair coins are tossed.
+
+def question_two():
+    s = 2 ()
